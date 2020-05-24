a1523f61ede04756c01bf3b21a0c29e379a6a3a2
Luis Aragon <lmaragon@gmail.com>
2020-05-24 16:35:36 -0700

    Create basic calculator layout

---
+++ app.js
@@ -0,0 +1,21 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+const operate = (func, a, b) => {
+    return func(a, b);
+}
+
+console.log(operate(multiply, 4, 5));
